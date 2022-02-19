ce93dd924d90b6065c841be6d8a8b2c0edc4ee30
timwolf8503 <96329797+timwolf8503@users.noreply.github.com>
2022-02-19 17:04:16 +0800

    Update 屬性判斷式

--- 2DRuanning_2021_12_18/Assets/Script/Learncondition.cs
+++ 2DRuanning_2021_12_18/Assets/Script/Learncondition.cs
@@ -4,6 +4,8 @@ using UnityEngine.PlayerLoop;
 
 /// <summary>
 /// 學習判斷式
+/// 1.if
+/// 2.switch
 /// </summary>
 
 public class Learncondition : MonoBehaviour
@@ -17,10 +19,24 @@ public class Learncondition : MonoBehaviour
 
     public bool haskey =false;
     public int combo = 0;
+    public string weapon;
 
+
+    //列舉public  enum 名稱{選單名稱}
+    //1.製作列舉(下拉選單)
+    public enum Season
+    {
+        spring,summer,fall,winter   
+    }
+
+    //2.實作列舉
+    public Season season;
 
     private void Start()
     {
+        #region if判斷式
+
+     
         if (true)
         {
             print("我式判斷式if");
@@ -39,19 +55,78 @@ public class Learncondition : MonoBehaviour
         {
 
             print("不能開門");
+
+            #endregion
+
+            #region swith判斷式
+
+
+
+            //swith語法()
+            //{
+            //  case    值1:
+            //            程式內容;
+            //            程式內容;
+            //            程式內容;
+            //            程式內容;
+            //          斷開break;
+            //  default
+            //            程式內容;
+            //            程式內容;
+            //            程式內容;
+            //            程式內容;
+            //          斷開break;
+            //}
+            switch (weapon)
+            {
+                case "小刀":
+                    print("攻擊力10");
+                        break;
+                case "弓箭":
+                    print("攻擊力100");
+                    break;
+                case "手槍":
+                    print("攻擊力200");
+                    break;
+                default:
+                    print("沒有武器");
+                    break;
+            }
+            #endregion
+
+
+            //swith +   enum
+            switch (season)
+            {
+                case Season.spring:
+                    print("春天");
+                    break;
+                case Season.summer:
+                    print("夏天");
+                    break;
+                case Season.fall:
+                    print("秋天");
+                    break;
+                case Season.winter:
+                    print("冬天");
+                    break;
+            
+            }
+
+
         }
 
         //持續進行方式
         //else  if(布林值){程式，演算法，陳述式}
         //必須寫在if下方
         //數量沒有限制
-        
-   
+
+
 
 
 
     }
-
+    
     private  void Update() 
     {
         if (combo == 10)

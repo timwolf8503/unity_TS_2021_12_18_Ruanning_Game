a1acbc3a855a8643f5f0196415bbb15eec78c8b7
timwolf8503 <96329797+timwolf8503@users.noreply.github.com>
2022-03-05 17:04:10 +0800

    Update-玩家跑步與跳躍程式

--- 2DRuanning_2021_12_18/Assets/Script/Plaryer.cs
+++ 2DRuanning_2021_12_18/Assets/Script/Plaryer.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 /// <summary>
-/// 橫向卷軸玩家的跑褲遊戲系統
+/// 橫向卷軸玩家的跑酷遊戲系統
 /// </summary>
 public class Plaryer : MonoBehaviour
 {
+    #region 欄位
+
 
     //跑步速度，跳躍高度，是否滑行，是否死亡
     //動畫參數跳躍，滑行與死亡
@@ -13,7 +15,7 @@ public class Plaryer : MonoBehaviour
     [Header("跑步速度"),Range(0,500)]
     public float speed = 1.5f;
     [Header("跳躍高度"),Range(0,5000)]
-    public int Jump = 500;
+    public int jump = 500;
     [Header("是否滑行")]
     public bool isSlide;
     [Header("是否滑行")]
@@ -23,5 +25,92 @@ public class Plaryer : MonoBehaviour
     public string parameterisSlide = "開關滑行";
     public string parameterisDead = "觸發死亡";
 
+
+    [Header("跳躍按鍵")]
+    public  KeyCode KeyJump= KeyCode.Space;
+
+    [Header("跳躍段數最大值"),Range(0, 5)]
+    public int countjumpMax = 2;
+    public int countjump;
+
+    //存取Transform第一種方式
+    //public Transform traPlayer;
+
+
+    //屬性面板...Debug模式可以看到私人資料
+    private Rigidbody2D rig;
+    
+
+    #endregion
+
+
+
+
+
+    #region 事件
+    private void Start()
+    {
+
+        // GetComponent<元件類型>();        所有元件
+        rig = GetComponent<Rigidbody2D>();
+       
+    }
+
+
+
+
+    private void Update()
+    {
+        Run();
+        Jump();
+    }
+
+
+    #endregion
+
+
+
+    #region 方法
+    /// <summary>
+    /// 跑步
+    /// </summary>
+    private void Run()
+    {
+
+
+        //存取Transform第一種方式
+        //Time.delatTime 一幀的時間
+        //traPlayer.Translate(speed*Time.deltaTime,0,0);         //玩家變形 位移(X,Y,Z)
+
+
+        //存取Transform第二種方式
+        //transform.名稱;
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+       bool intJump= Input.GetKeyDown(KeyJump);
+        //print("是否跳躍" + intJump);
+
+
+        //如果按下按鍵    並且跳躍段數大於 0 往上跳
+        if (intJump && countjump>0 )
+        {
+            // print("跳躍");
+            //鋼體添加推力
+            rig.AddForce(new Vector2(0, jump));
+            countjump--;
+        }
+    }
+
+
+
+
+    
+    #endregion
 
 }
--- 2DRuanning_2021_12_18/Assets/Script/SceneConrol.cs
+++ 2DRuanning_2021_12_18/Assets/Script/SceneConrol.cs
@@ -10,6 +10,22 @@ using UnityEngine.SceneManagement;      //引用場景管理
 public class SceneConrol : MonoBehaviour
 {
     
+
+    /// <summary>
+    /// 延遲開始有遊戲
+    /// </summary>
+ public void DelayLoadScene()
+    {
+        //有MonoBehehaviour語法
+        //方法名稱(對應引數);
+        Invoke("LoadSence", 0.5f);      //延遲呼叫  (方法名稱,延遲秒數)
+
+
+
+    }
+
+
+
     /// <summary>
     /// 開始遊戲
     /// </summary>
@@ -20,16 +36,28 @@ public class SceneConrol : MonoBehaviour
 
     }
 
+
+
+
+
+
+
+    //延遲離開遊戲
+    public void DelayQuitGame()
+    {
+        Invoke("QuitGame", 0.5f);
+    }
+
     //Unity離開遊戲
     //1.公開此方法
     //2.Button元件 on cick 可以指定此方法
-   /// <summary>
-   /// 離開遊戲
-   /// </summary>
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
     public void QuitGame()
     {
         print("離開遊戲");
-        Application.Quit();     //應用程式離開
+        Application.Quit();     //應用程式離開  Unity裡不會執行
 
     }
 
